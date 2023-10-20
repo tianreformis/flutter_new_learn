1edeaad721b04f489fdd5ad3e98ab4db5ee3a473
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2023-10-20 16:09:29 +0800

    add widget learning

--- 00. dart_basic/tianreformis_lesson_material/lib/main.dart
+++ 00. dart_basic/tianreformis_lesson_material/lib/main.dart
@@ -9,10 +9,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+          title: const Text('Widget Dasar'),
+          leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.person,
+              )),
+        ),
+        body: const Center(
           child: Text('Hello World!'),
+        ),
+        bottomNavigationBar: const BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.home),
+              Icon(Icons.home),
+              Icon(Icons.home),
+              Icon(Icons.home),
+            ],
+          ),
         ),
       ),
     );
